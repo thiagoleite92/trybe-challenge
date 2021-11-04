65b44a370e2553178921a27a33270b3d37dc7883
Thiago Leite <leite.tjs@gmail.com>
2021-11-04 16:21:30 -0300

    working tasks model

---
+++ backend/models/tasks.js
@@ -0,0 +1,15 @@
+const connection = require('./connection');
+
+const getTasksList = async () => {
+  const db = await connection();
+
+  const tasksList = await db.collection('tasks').find().toArray();
+
+  return tasksList;
+};
+
+const insertTask = async (task) => {
+  const db = await connection();
+
+  await db.collection('tasks').insertOne({ task });
+};
